6bc94ee22bb142701c50e70e650d68cb2f7c890a
mariaeduardacosta9090 <maria.eduardasilvaeufrazio.costa@escola.pr.gov.br>
2024-08-08 08:35:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [
+    {
+        enunciado:"Pergunta 1",
+        alternativas:["Alternativa 1","Alternativa 2"
+        ]
+        
+    },
+    {
+        enunciado:"Pergunta 1",
+        alternativas:["Alternativa 1","Alternativa 2"
+        ]
+        
+    },
+    {
+        enunciado:"Pergunta 1",
+        alternativas:["Alternativa 1","Alternativa 2"
+        ]
+        
+    }
+]
+
+let atual=0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    
+}
