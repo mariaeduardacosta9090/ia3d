aa4b089fcbb092a18ba7d6d4451a701d63192ce0
mariaeduardacosta9090 <maria.eduardasilvaeufrazio.costa@escola.pr.gov.br>
2024-10-10 08:38:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,31 +2,73 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector ('.texto-reultao');
 
 const perguntas = [
     {
-        enunciado:"Pergunta 1",
-        alternativas:["Alternativa 1","Alternativa 2"
+        enunciado: "Você gosta da ideia de Inteligência Artificial?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmativa: "IA é muito usada hoje em dia"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
         ]
-        
     },
     {
-        enunciado:"Pergunta 1",
-        alternativas:["Alternativa 1","Alternativa 2"
+        enunciado: "Voce gosta da IA?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
         ]
-        
     },
     {
-        enunciado:"Pergunta 1",
-        alternativas:["Alternativa 1","Alternativa 2"
+        enunciado: "Voce usa a IA?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
         ]
-        
     }
 ]
 
-let atual=0;
+let posicao = 0;
 let perguntaAtual;
+let respostas = "";
 
-function mostraPergunta(){
-    
+function mostraPergunta() {
+    perguntaAtual = perguntas[posicao];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
+        caixaAlternativa.appendChild(botaoAlternativas);
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmativa;
+    respostas = afirmacoes;
+    posicao++;
+    mostraPergunta();
+}
+function mostraResposta  
+   caixaPergunta.textContent = "Em 25 anos ...";
+mostraPergunta();
